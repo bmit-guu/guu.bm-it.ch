02c7abc22b2cbdbf3a809d1a926c68a8ca21d006
bmit-guu <ulas.guevenc@espas.ch>
2024-03-11 16:51:22 +0100

    v1.0.0

--- indexjs.js
+++ indexjs.js
@@ -2,20 +2,20 @@ function uebermich() {
     var contentContainer = document.getElementById("GridR");
   
     var contentHTML = `
-      <h1 style="font-size:54px; color: #000000">Über mich<h1><br>
-      <img src="./portfolio.svg">
-      <p style="color: #000000; font-size: 24px;">
-      <b>Name Nachname: </b>Ulas Güvenç<br><br>
-      <b>Geburtsdatum: </b>8. Juli 2003<br><br>
-      <b>Ausbildung:</b><br>
-      2009-2016: Primar- und Oberstufenschule (Türkei)<br>
-      2016-2020: Sekundarschule<br>
-      2020-2021: 10. Schuljahr, KSB Aarau<br>
-      2021-2021: Praktikum als Konstrukteur EFZ<br>
-      2022-2022: Vorlehre ICT-Fachmann EFZ<br>
-      2022-2023: Vorlehre Informatiker EFZ<br>
-      2023-heute: 1. Lehrjahr als Informatiker EFZ, Applikationsentwicklung
-      </p>
+    <h1 class="inhaltsTitel">Über mich<h1><br>
+    <img style="max-width: 50%; height: auto;" src="./portfolio.svg">
+    <p class="inhalt">
+    <b>Name, Vorname: </b>Güvenç Ulas<br><br>
+    <b>Geburtsdatum: </b>8. Juli 2003<br><br>
+    <b>Ausbildung:</b><br>
+    2009-2016: Primar- und Oberstufenschule (Türkei)<br>
+    2016-2020: Sekundarschule<br>
+    2020-2021: 10. Schuljahr, KSB Aarau<br>
+    2021-2021: Praktikum als Konstrukteur EFZ<br>
+    2022-2022: Vorlehre ICT-Fachmann EFZ<br>
+    2022-2023: Vorlehre Informatiker EFZ<br>
+    2023-heute: 1. Lehrjahr als Informatiker EFZ, Applikationsentwicklung<br>
+    </p>
     `;
   
     contentContainer.innerHTML = contentHTML;
